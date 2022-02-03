afc86421603d656564e9158bca359820b5cb3cc8
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-03 14:59:41 +0100

    green

--- src/main/java/example/DancingPlayer.java
+++ src/main/java/example/DancingPlayer.java
@@ -1,9 +1,11 @@
 package example;
 
 public class DancingPlayer extends Player {
+	private int remainingTurns = 3;
 	@Override
 	protected Player execute(Command command) {
-		if (command == Command.STOP)
+		remainingTurns--;
+		if (command == Command.STOP || remainingTurns == 0)
 			return new IdlePlayer();
 		return this;
 	}
