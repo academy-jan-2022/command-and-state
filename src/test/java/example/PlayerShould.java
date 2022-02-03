2d32fefb388603e9d73ca057f63c4072d6b42746
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-03 12:22:10 +0100

    red

--- src/test/java/example/PlayerShould.java
+++ src/test/java/example/PlayerShould.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerShould {
 	@Test void
-	become_idle_on_stop() {
+	become_idle_on_stop_when_dancing() {
 		Player player = new DancingPlayer();
 		player.queue(STOP);
 		var result = player.execute();
 		assertInstanceOf(IdlePlayer.class, result);
+	}
+
+	@Test void
+	keep_dancing_when_told_to_attack() {
+		Player player = new DancingPlayer();
+		player.queue(ATTACK);
+		var result = player.execute();
+		assertInstanceOf(DancingPlayer.class, result);
 	}
 }
