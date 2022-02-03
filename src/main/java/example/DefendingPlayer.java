63e1baef5f58cb122e998a4ed7cbe321715ce987
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-03 12:41:54 +0100

    refactor

--- src/main/java/example/DefendingPlayer.java
+++ src/main/java/example/DefendingPlayer.java
@@ -5,10 +5,8 @@ public class DefendingPlayer extends Player {
 
 	@Override
 	protected Player execute(Command command) {
-		if (command == Command.STOP)
-			return new IdlePlayer();
 		turnsLeft--;
-		if (turnsLeft == 0)
+		if (command == Command.STOP || turnsLeft == 0)
 			return new IdlePlayer();
 
 		return this;
