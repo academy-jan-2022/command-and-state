4455d90a197f846b7e4e922302fcc7d954092e47
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-03 12:41:34 +0100

    green

--- src/main/java/example/DefendingPlayer.java
+++ src/main/java/example/DefendingPlayer.java
@@ -1,10 +1,16 @@
 package example;
 
 public class DefendingPlayer extends Player {
+	private int turnsLeft = 2;
+
 	@Override
 	protected Player execute(Command command) {
 		if (command == Command.STOP)
 			return new IdlePlayer();
+		turnsLeft--;
+		if (turnsLeft == 0)
+			return new IdlePlayer();
+
 		return this;
 	}
 }
