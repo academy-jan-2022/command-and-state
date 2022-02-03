d8be8b835366ae35fa3c40e0c8e94db92bec0f47
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-03 12:26:24 +0100

    green

--- src/main/java/example/IdlePlayer.java
+++ src/main/java/example/IdlePlayer.java
@@ -3,6 +3,8 @@ package example;
 public class IdlePlayer extends Player {
 	@Override
 	protected Player execute(Command command) {
+		if (command == Command.DANCE)
+			return new DancingPlayer();
 		return null;
 	}
 }
